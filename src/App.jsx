0cf43de2196650d7a20dfe7f686e0024b58aca7c
Clare <clareivers@gmail.com>
2024-08-24 10:22:26 +1000

    more styling and layout fixes

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,22 @@ function App() {
     <div className="app-container">
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
-        <div className="content">
-          <div className="landing_content">
-            <h1>Welcome To Paradise Nursery</h1>
-            <div className="divider"></div>
-            <p>Where Green Meets Serenity</p>
+        <div className="backdrop-filter">
 
-            <button className="get-started-button" onClick={handleGetStartedClick}>
-              Get Started
-            </button>
+          <div className="content">
+            <div className="landing_content">
+              <h1>Welcome To Paradise Nursery</h1>
+              <div className="divider"></div>
+              <p>Where Green Meets Serenity</p>
+
+              <button className="get-started-button" onClick={handleGetStartedClick}>
+                Get Started
+              </button>
+            </div>
+            <AboutUs />
           </div>
-          <AboutUs />
-        </div>
 
+        </div>
       </div>
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
         <ProductList />
